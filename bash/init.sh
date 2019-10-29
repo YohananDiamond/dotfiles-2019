d8165f9101218d4d83c924315e01514684691e2c
yohanandiamond <mitonanan12@gmail.com>
2019-10-28 23:59:07 -0300

    Bash reorganization and more.
    I have reorganized my bash prompt and it looks a bit better (and smaller) now.
    I also moved all old emacs data to a folder called .unused.

--- bash/init.sh
+++ bash/init.sh
@@ -1,41 +1,68 @@
 #!/usr/bin/env bash
-# Bash Dotfiles
-# Author: YohananDiamond
-# 
-# Note: most custom commands are under the "wd" command
-# and most options are under the "wdx" command.
-#
-# (TODO) Finish NA
 
-#############
-# CONSTANTS #
-#############
+# CONSTANTS ######################################
 
-# The directory of this path
-WD_InitPath=$(dirname $BASH_SOURCE)
+export INITFILES=$(dirname $BASH_SOURCE)
+export DOTFILES=$(dirname $INITFILES)
+export COLUMNS # Make all processes know the amount of columns
 
-# Make all processes know the amount of columns on the screen
-export COLUMNS
-
-# Current Platform
+export PLATFORM="std"
 if [[ -r /sdcard ]]; then
-	WD_Platform="termux"
+  export PLATFORM="termux"
 elif [[ -r "/mnt/c/Windows" ]]; then
-	WD_Platform="wsl"
-else
-	WD_Platform="regular"
+  export PLATFORM="wsl"
 fi
 
-#############
-# INIT VARS #
-#############
+# HEADER FILES ###################################
+
+. $INITFILES/header.sh
+
+# INIT VARS ######################################
 
 # Bash "first run in session" code
-[[ -z "$WD_FirstRun" ]] && WD_FirstRun=0
+[[ -z "$FIRST" ]] && FIRST=0
 
-#############
-# FUNCTIONS #
-#############
+# ALIASES ########################################
+
+alias rbash='source $HOME/.bashrc'
+alias la='ls -A $@'
+alias ll='ls -alF $@'
+alias l='ls -CF $@'
+alias cl='cd $@ && la'
+alias py3='python3 $@'
+alias ipy3='ipython $@'
+alias du='du -shc'
+alias rscp='rsync -aP'
+alias rsmv='rsync -aP --remove-source-files'
+
+# Git aliases
+alias gh='echo ALIASES -> gs gpp gc gl'
+alias gpp='git pull && git push'
+alias gs='git status --short'
+alias gc='git add . && git commit'
+alias gl='git log --oneline'
+
+# APP THEMING ####################################
+
+# "less" config
+export LESS_TERMCAP_mb=$'\E[1;31m'     # begin bold
+export LESS_TERMCAP_md=$'\E[1;35m'     # begin blink
+export LESS_TERMCAP_me=$'\E[0m'        # reset bold/blink
+export LESS_TERMCAP_so=$'\E[33m'       # begin reverse video
+export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
+export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
+export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
+
+# prompt config
+PS1=$(py3 $INITFILES/bin/prompt)
+PS2='\[\033[1;37;45m\] ... \[\033[m\] '
+
+# PATH ###########################################
+
+pathappend "$HOME/.local/bin"
+pathappend "$INITFILES/bin"
+
+#
 
 # A temporary git thing. Not very easy to use.
 g() {
@@ -56,74 +83,14 @@ g() {
     fi
 }
 
-# import: run .sh files inside this directory.
-import() {
+# FIRST LOAD CODE ###############################
 
-	# Function name for error prompting
-	local FunctionName="import"
+if [[ $FIRST == 0 ]]; then
 
-	# Verify if the input is okay
-	[[ "$1" == "" ]] && echo "$FunctionName: first argument empty" \
-		&& return 1;
-	
-	# Check if the file exists and run it
-	local FilePath="${WD_InitPath}/${1}.sh"
-	[[ -f "${FilePath}" ]] && source "${FilePath}" \
-		|| echo "$FunctionName: ${FilePath} not found" && return 1;
+    FIRST=1
 
-}
-
-# Run a tmux session or restore it
-tmx() {
-	[[ -n $1 ]] \
-		&& (tmux attach -t $1 $2 &>/dev/null \
-			|| tmux new-session -s $1 \; source-file ${WD_InitPath}/../tmux/sessions/$1.proj \; $2) \
-	|| echo 'Usage: tmx setup-name <optional command>'
-}
-
-# Send the quit command to all panes/windows/sessions on tmux
-tquit() {
-		tmux list-panes -a -F '#{session_name}:#{window_index}.#{pane_index}' \
-				| xargs -I PANE tmux send-keys -t PANE 'C-c' 'C-d' Enter
-}
-
-##############
-# OUTER CODE #
-#  RUNNING   #
-##############
-
-# Add a scripts path to $PATH
-[[ $PATH =~ (^|:)${WD_InitPath}/tools(/?)(:/$) ]] \
-	|| PATH="$PATH:${WD_InitPath}/tools"
-
-# Platform-based
-if [[ ${WD_Platform} == "wsl" ]]; then
-	import "platform/wsl"
-	import "sets/tr"
-elif [[ ${WD_Platform} == "termux" ]]; then
-	import "platform/termux"
-fi
-
-# Any platform
-import "sets/aliases"
-import "sets/utilfuncs"
-import "sets/appsets"
-
-#############
-# FINISHING #
-#############
-
-wdprompt # Run the code at the end of its definition
-
-# Code ran on the first time.
-if [[ $WD_FirstRun == 0 ]]; then
-
-	# Set the FirstRun var to true
-	WD_FirstRun=1
-
-	# Start the "back" and "main" tmux sessions.
-	[[ -z "$TMUX" ]] && [[ $PWD == $HOME ]] \
-		&& tmx back detach \
-		&& tmx main
+    # Start the "back" and "main" tmux sessions.
+    [[ -z "$TMUX" ]] && [[ $PWD == $HOME ]] \
+        && tmx back detach && tmx main
 
 fi
